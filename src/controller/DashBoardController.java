87b993e517375227337c4a181759c5b973459969
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-08-23 20:35:22 +0530

    date Time added

--- src/controller/DashBoardController.java
+++ src/controller/DashBoardController.java
@@ -1,13 +1,19 @@
 package controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 
 public class DashBoardController implements Initializable {
 
@@ -39,6 +45,24 @@ public class DashBoardController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        manageDateAndTime();
+    }
 
+    private void manageDateAndTime() {
+        Timeline date = new Timeline(new KeyFrame(
+                Duration.ZERO,
+                actionEvent -> lblDate.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+        ), new KeyFrame(Duration.seconds(1)));
+
+        date.setCycleCount(Animation.INDEFINITE);
+        date.play();
+
+        Timeline time = new Timeline(new KeyFrame(
+                Duration.ZERO,
+                actionEvent -> lblTime.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")))
+        ), new KeyFrame(Duration.seconds(1)));
+
+        time.setCycleCount(Animation.INDEFINITE);
+        time.play();
     }
 }

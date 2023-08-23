40ca9a4fc91c964205bd88bc6cbd3d677361638e
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-08-23 20:22:44 +0530

    initial commit

---
+++ src/AppInitializer.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/*
+* JDK 11
+* Build System - IntelliJ
+* jfoenix -9.0.1
+* javafx-SDK 19.0.2.1
+* JavaFx Project*/
+
+public class AppInitializer extends Application { //javafx.application  // alt+enter --> implement methods
+    public static void main(String[] args) { //psvm -> tab
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/DashBoard.fxml"))));
+        /*URL resource = getClass().getResource("view/DashBoard.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Scene scene = new Scene(load);
+        stage.setScene(scene);*/
+
+        stage.show();
+    }
+}
---
+++ src/controller/DashBoardController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class DashBoardController implements Initializable {
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    void customerButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void itemButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void orderButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void placeOrderButtonOnAction(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
